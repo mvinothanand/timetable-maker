360f90e9c69750623d3da155b923833843ea383b
vinoth masilamani <vinoth.anand@gmail.com>
2023-08-12 21:51:43 +0530

    updates

--- main.py
+++ main.py
@@ -21,6 +21,47 @@ def get_availablity_bitmap(input_string, separator = "*"):
   return bitmap
 
 
+def get_merged_bm_week(staff_records):
+  staff_merged_bm_week = []
+
+  for i,day in zip(range(5), ["Mon", "Tue", "Wed", "Thu", "Fri"]):
+    staff_avail_bm_day = []
+    for staff in staff_records:
+      #print(staff["availability_bitmap"])
+      staff_avail_bm_day.append(staff["availability_bitmap"][i])
+    #print(f"Day {i + 1} availability bm: {staff_avail_bm_day}")
+    staff_day_merged_bm = get_merged_bm_day(staff_avail_bm_day)
+    #print(f"{day} merged bm: {staff_day_merged_bm}")
+    staff_merged_bm_week.append(staff_day_merged_bm)
+  
+  return staff_merged_bm_week
+
+def get_merged_bm_day(input_list):
+  merged_bm_day = ""
+  for i in range(8):
+    result = 0
+    for item in input_list:
+      result += int(item[i])
+    #print(f"{i} {result}")
+    merged_bm_day = str(merged_bm_day) + str("0" if result == 0 else "1")
+    #print(day_merged_bm)
+  return merged_bm_day
+
+
+# find a slot for a course
+# looks at the staff avaiability
+def get_slot(course, staff_availability):
+  print(f"finding slot for {course['name']}")
+  print([staff['name'] for staff in staff_availability])
+  # duration = course['max_block_size']
+  # while duration >= course['min_block_size']:
+  #   required_bitmap = duration * "0"
+  #   day_counter = 1
+  #   for day_bm in staff['availability_bitmap']:
+  #     if match_slot := day_bm.find(required_bitmap):
+  #       return match_slot
+  #   duration -= 1
+
 
 def main():
   # Validate input
@@ -44,26 +85,30 @@ def main():
   #print(staff_availability)
 
   # Parse the daily availability and enrich the staff_availability dictionary
+  print("\nstaff availability before allocation: ")
   for staff in staff_availability:
     staff["availability_bitmap"] = []
     for day in ["Mon","Tue","Wed", "Thu", "Fri"]:
-      #print(f"{staff['name']} availability on {day}: {staff[day]}")
-      availability_string = staff[day]
+      #print(f"{staff['name']} availability on {day}: ")
+      availability_string = staff[day]  
       availability_dict = {hour:availability for (hour,availability) in zip(range(1,9), staff[day].split("*"))}
       #print(day_availability)
       staff[day] = availability_dict
       availability_bitmap = get_availablity_bitmap(availability_string,"*")
-      print(availability_bitmap)
+      #print(availability_bitmap)
       staff["availability_bitmap"].append(availability_bitmap)
     #print("\n")
-
-  print("\nstaff availability before allocation: ")
-  print(staff_availability)
+    print(f"{staff['name']}: {staff['availability_bitmap']}")
+ 
+  #print(staff_availability)
 
   # Find a slot for each course
-  # for course in sorted(course_details,key = lambda course: course["min_block_size"], reverse = True):
-  #   print(f"{course['class']} {course['name']}")
-
+  for course in sorted(course_details,key = lambda course: course["max_block_size"], reverse = True):
+    print(f"{course['class']} {course['name']} {course['staff']}")
+    staff_records = list(filter(lambda staff: staff['name'] in course['staff'].split("|"),staff_availability))
+    staff_curr_avail_bmap = get_merged_bm_week(staff_records)
+    print(f"Staff Availability bmap: {staff_curr_avail_bmap}")
+    #mapped_slot = get_slot(course, staff_records)
 
 
 if __name__ == "__main__":
