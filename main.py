d46bf85ae2d989a6d5a25b24663ca43467ba268d
vinoth masilamani <vinoth.anand@gmail.com>
2023-08-14 21:49:13 +0530

    updates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from colorama import Fore, init
 
 from common import (
   csvToList,
@@ -8,6 +9,8 @@ from config import (
   course_details_file,
   staff_availability_file
 )
+
+init(autoreset=True)
 
 
 def get_availablity_bitmap(input_string, separator = "*"):
@@ -48,58 +51,45 @@ def get_merged_bm_day(input_list):
   return merged_bm_day
 
 
+def get_slot(rem_hours, block_size, day_avail_bmap):
+  slot_size = min(rem_hours, block_size)
+  reqd_bmap = slot_size * "0"
+  matched_slot = day_avail_bmap.find(reqd_bmap)
+  return {"hour": matched_slot + 1, "slot_size": slot_size} if matched_slot >= 0 else None
+
+
 def find_slots(course, staff_avail_merged_bmap):
+  #initializing required variables
   rem_hours = int(course["weekly_hours"])
   max_block_size = int(course["max_block_size"])
   min_block_size = int(course["min_block_size"])
   matched_slots = {"Mon": [], "Tue": [], "Wed": [], "Thu": [], "Fri": []}
+
   for i, day in zip(range(5), ["Mon", "Tue", "Wed", "Thu", "Fri"]):
     if rem_hours <= 0:
       break
-    # first check if there is a slot for max_block_size
-    slot_size = min(rem_hours, max_block_size)
-    reqd_bmap = slot_size * "0"
-    matched_slot = staff_avail_merged_bmap[i].find(reqd_bmap)
-    print(matched_slot)
-    if matched_slot >= 0:
-      #matched_slots.append({day: [{"hour": matched_slot + 1, "slot_size": slot_size}]})
-      matched_slots[day].append({"hour": matched_slot + 1, "slot_size": slot_size})
-      rem_hours = rem_hours - slot_size
-    else: 
-      if max_block_size > min_block_size:
-        # Check if there is a slot or min block size
-        slot_size = min(rem_hours, min_block_size)
-        reqd_bmap = slot_size * "0"
-        matched_slot = staff_avail_merged_bmap[i].find(reqd_bmap)
-        if matched_slot >= 0:
-          matched_slots[day].append({"hour": matched_slot + 1, "slot_size": slot_size})
-          rem_hours = rem_hours - slot_size
+    curr_bmap = staff_avail_merged_bmap[i]
+    # first check for max block size else for min_block_size
+    matched_slot = get_slot(rem_hours, max_block_size,curr_bmap)
+    if (not matched_slot) and min_block_size < max_block_size:
+      matched_slot = get_slot(rem_hours, min_block_size,curr_bmap)
+
+    if matched_slot:
+      matched_slots[day].append(matched_slot)
+      rem_hours = rem_hours - matched_slot["slot_size"]
+  
   return matched_slots
 
-
-# find a slot for a course
-# looks at the staff avaiability
-def get_slot(course, staff_availability):
-  print(f"finding slot for {course['name']}")
-  print([staff['name'] for staff in staff_availability])
-  # duration = course['max_block_size']
-  # while duration >= course['min_block_size']:
-  #   required_bitmap = duration * "0"
-  #   day_counter = 1
-  #   for day_bm in staff['availability_bitmap']:
-  #     if match_slot := day_bm.find(required_bitmap):
-  #       return match_slot
-  #   duration -= 1
 
 
 def main():
   # Validate input
   if len(sys.argv) < 2:
-    sys.exit("Insufficient inputs")
+    sys.exit(Fore.RED + "Insufficient inputs")
   
   # Read the class for which the timetable is to be prepared
   sem = sys.argv[1]
-  print(f"Timetable to be prepared for {sem}")
+  print(f"{Fore.CYAN}Timetable to be prepared for {sem}")
 
   # Get the course details for the class
   course_details_all = csvToList(course_details_file)
@@ -114,7 +104,7 @@ def main():
   #print(staff_availability)
 
   # Parse the daily availability and enrich the staff_availability dictionary
-  print("\nstaff availability before allocation: ")
+  print(Fore.CYAN + "\nStaff availability before allocation: ")
   for staff in staff_availability:
     staff["availability_bitmap"] = []
     for day in ["Mon","Tue","Wed", "Thu", "Fri"]:
@@ -135,14 +125,16 @@ def main():
   course_slot_mapping = []
   counter = 1
   for course in sorted(course_details,key = lambda course: course["max_block_size"], reverse = True):
-    print(f"{course['class']} {course['name']} {course['staff']}")
+    print(f"\n{Fore.CYAN}{course['class']} {course['name']} {course['staff']}")
     staff_records = list(filter(lambda staff: staff['name'] in course['staff'].split("|"),staff_availability))
     staff_curr_avail_bmap = get_merged_bm_week(staff_records)
-    print(f"Merged Staff Availability bmap for the week : {staff_curr_avail_bmap}")
+    print(f"{Fore.GREEN}Merged Staff Availability bmap for the week : {staff_curr_avail_bmap}")
     mapped_slots = find_slots(course, staff_curr_avail_bmap)
     print(mapped_slots)
+    # update staff availability
+    update_staff_availability(staff_records)
     counter += 1
-    if counter > 2:
+    if counter > 3:
       break
 
 
